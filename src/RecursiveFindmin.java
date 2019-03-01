7789a9d9adbc7a66a9ad6f66c491954f55e619f9
puth1998 <teenaputh12@hotmail.com>
2019-03-01 12:01:24 +0700

    create src

---
+++ src/RecursiveFindmin.java
@@ -0,0 +1,30 @@
+public class RecursiveFindmin {
+	public static void main(String[] args) {
+		  int A[] = {16, 4, 45, 6, 50, 10, 2}; 
+//	        int n = A.length; 
+	          
+	        // Function calling 
+	        int m = findMinRec(A,0,A.length-1); 
+	        System.out.println(m);
+		
+	}
+	  // function to return minimum element using recursion 
+    public static int findMinRec(int A[],int startIndex, int endIndex) 
+    { 
+    
+      if(startIndex == endIndex) 
+        return A[startIndex]; 
+      
+      int mid = (startIndex+endIndex)/ 2;
+      
+      int m1 = findMinRec(A,startIndex,mid);
+      
+      int m2 = findMinRec(A,mid+1,A.length-1);
+      
+     if(m1 < m2 ) {
+    	 return m1;
+     }
+       	return m2; 
+    } 
+    
+}
