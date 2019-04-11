32fd2c44cedf539613dd1c82114bf42db5b6656f
Puth1998 <Teenaputh12@hotmail.com>
2019-04-11 11:39:10 +0700

    modifine has majority

--- src/hasMajority.java
+++ src/hasMajority.java
@@ -1,25 +1,21 @@
 import java.util.Random;
 import java.util.Scanner;
-
 public class hasMajority {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner (System.in);
 		Random rnd= new Random();
 		int[] A = new int[11];
 		int q;
-		int k = 2;
+		int k = 1;
 		int c = 0; 
-		int m = 0;
-		
+		int m = 0;	
 //		3 2 2 3 5 6 1 2 2 2 2
 		System.out.print("Input : ");
 		for(int p = 0; p < A.length; p++) {	
 			q = in.nextInt();
 			A[p] = q;
 		}
-	
 		for ( int j = 0 ; j < k ; j++) {
 			c = 0;
 			m = (A[rnd.nextInt(A.length)]);
